b9dc79f67c330e589505863f345f774178b51b1f
KorsK <kdong@my.chemeketa.edu>
2017-10-17 18:10:08 -0700

    Add files via upload

---
+++ j3dMain.java
@@ -0,0 +1,144 @@
+import java.awt.BorderLayout;
+import java.awt.GraphicsConfiguration;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.media.j3d.Background;
+import javax.media.j3d.BoundingSphere;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Canvas3D;
+import javax.media.j3d.ImageComponent2D;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.media.j3d.View;
+import javax.swing.JFrame;
+import javax.vecmath.Point3d;
+
+import com.sun.j3d.utils.universe.SimpleUniverse;
+import com.sun.j3d.utils.universe.Viewer;
+import com.sun.j3d.utils.universe.ViewingPlatform;
+
+@SuppressWarnings("serial")
+public class j3dMain extends JFrame implements KeyListener{
+
+	private SimpleUniverse u;
+	private BranchGroup g;
+	private boolean running;
+	private Viewer viewer;
+	private View view;
+	private ViewingPlatform viewP;
+	private j3dPerson player;
+	@SuppressWarnings("unused")
+	private static Transform3D viewt3d;
+	@SuppressWarnings("unused")
+	private static TransformGroup viewTG;
+	int xDim;
+	int yDim;
+	int zDim;
+	
+	public j3dMain() throws Exception{
+		
+		super();
+		init();
+		running = true;
+		Thread t = new Thread(new running());
+		t.start();
+	}
+	
+	public void init() throws Exception{
+		
+		getContentPane().setLayout(new BorderLayout());
+		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+		Canvas3D canvas = new Canvas3D(config);
+		getContentPane().add(canvas, BorderLayout.CENTER);
+		
+		
+		viewer = new Viewer(canvas);
+		view = viewer.getView();
+		view.setBackClipDistance(10);
+		
+		
+		viewP = new ViewingPlatform();
+		viewP.getViewPlatform().setActivationRadius(300f);
+		
+		viewTG = viewP.getViewPlatformTransform();
+		viewt3d = new Transform3D();
+		
+		BufferedImage bg= ImageIO.read(new File("res/stars.jpg"));
+		ImageComponent2D img2d = new ImageComponent2D(1,bg);
+		Background back = new Background(img2d);
+		
+		BoundingSphere bgSphere = new BoundingSphere(new Point3d(0,0,0), 10000f);
+		back.setApplicationBounds(bgSphere);
+		
+		viewP.addChild(back);
+		
+		u = new SimpleUniverse(viewP,viewer);
+		u.getCanvas().addKeyListener(this);
+		g = new BranchGroup();
+		
+		xDim = 50;
+		yDim = 50;
+		zDim = 50;
+		
+		j3dLoadHills.loadHills(xDim,yDim,zDim, 25, 10, 5, 1, 3, 3, 3, 1, 1, 1, 1, g);
+		//j3dLoadSet.loadSet(xDim, yDim, zDim, g);
+		
+		player = new j3dPerson(viewP,yDim,0);
+		u.addBranchGraph(g);
+		
+	}
+	
+	public class running implements Runnable{
+		public void run(){
+			while(running){
+				try{
+					Thread.sleep(30);
+					player.update();
+				}
+				catch(Exception e){
+					
+				}
+			}
+		}
+	}
+	
+	
+	public static void setViewt3d(Transform3D newt3d){
+		viewt3d = newt3d;
+	}
+	
+	public static void setViewTG(TransformGroup newTG){
+		viewTG = newTG;
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		j3dMain frame = new j3dMain();
+		frame.setSize(500,500);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		if(arg0.getKeyCode() ==KeyEvent.VK_0){
+			System.exit(EXIT_ON_CLOSE);
+		}
+		player.keyPressed(arg0);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		player.keyReleased(arg0);
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {}
+}
---
+++ j3dPerson.java
@@ -0,0 +1,327 @@
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import com.sun.j3d.utils.universe.ViewingPlatform;
+
+
+public class j3dPerson{
+
+	Robot r;
+	private TransformGroup viewTrans;
+	private Transform3D viewt3d;
+	private Point3d playerCoords;
+	private Point3d playerLookAt;
+	private Vector3d playerUp;
+	private Vector3d collisionCoords;
+	private double mouseX, mouseY;
+	private double viewCircleTheta, upCircleTheta, viewCircleLength;
+	private double sensitivityX, sensitivityY;
+	private ArrayList<Integer> keyArr = new ArrayList<Integer>();
+	private keyRemove<Integer> filter = new keyRemove<Integer>();
+	private boolean collision, jumping, alreadyJumped;
+	private int yDim;
+	private double dy, maxY;
+	private ArrayList<ArrayList<ArrayList<Integer>>> Arr;
+	
+	public j3dPerson(ViewingPlatform vP, int y, int Maptype){
+		
+		dy = 0;
+		maxY = 1.0;
+	
+		if(Maptype ==0){
+			Arr = j3dLoadHills.getColl();
+		}
+		else{
+			Arr = j3dLoadSet.getColl();
+		}
+		
+		
+		jumping = alreadyJumped = false;
+		
+		yDim = y;
+		
+		
+		try {
+			r = new Robot();
+		} catch (AWTException e) {}
+		
+		sensitivityX=sensitivityY=20;
+		
+		playerCoords = new Point3d(30,100,50);
+		playerLookAt = new Point3d(0,0,0);
+		playerUp = new Vector3d(0,1,0);
+		collisionCoords = new Vector3d();
+		
+		viewCircleTheta = 0;
+		upCircleTheta = 0;
+		viewCircleLength = Math.abs(playerCoords.getZ() - playerLookAt.getZ());
+		
+		viewTrans = vP.getViewPlatformTransform();
+		viewt3d = new Transform3D();
+	}
+
+	public void update() {
+		collisionCoords.set((int)(playerCoords.getX()+1)/2,(int) (playerCoords.getY()-2)/2, (int)(playerCoords.getZ()+1)/2);
+		checkCollisions();
+		YUpdate();
+		viewUpdate();
+		KeyUpdate();
+		j3dMain.setViewt3d(viewt3d);
+		j3dMain.setViewTG(viewTrans);
+	}
+	
+	
+	public void checkCollisions(){
+		try{
+			if(Arr.get((int) collisionCoords.getZ()).get((int) (yDim-collisionCoords.getY())).get((int) collisionCoords.getX())!=0){
+				collision = true;
+			}
+			else{
+				collision = false;
+			}
+		}catch(Exception e){
+			collision = false;
+		}
+	}
+	
+	public void YUpdate(){
+		
+		if(jumping){
+			dy=maxY;
+			jumping=false;
+			alreadyJumped = true;
+			
+		}
+		else{	
+			if(collision){
+				dy=0;	
+				alreadyJumped = false;
+				
+			}
+			else{
+				 if(dy>-maxY){
+					 
+						dy-=0.1; 
+					 
+					 
+				 }
+				 else{
+					 dy=-maxY;
+				 }
+			}	
+		}
+		playerCoords.add(new Point3d(0,dy,0));
+		
+	}
+	
+	public void viewUpdate(){
+		
+		mouseY = MouseInfo.getPointerInfo().getLocation().y;
+		mouseX = MouseInfo.getPointerInfo().getLocation().x;
+		r.mouseMove(200, 200);
+	
+		double deltaX = (mouseX-200) / sensitivityX;
+		double deltaY = (mouseY-200) / sensitivityY;
+		
+		if(Math.abs(viewCircleTheta)> 2*Math.PI){
+			viewCircleTheta =0;
+		}
+		else{
+			viewCircleTheta += (double) (Math.atan((deltaX/viewCircleLength)));
+		}
+		if(Math.abs(upCircleTheta)> Math.PI/2){
+			if(upCircleTheta>0){
+				upCircleTheta =(float) (Math.PI/2);
+			}
+			else if(upCircleTheta<0){
+				upCircleTheta = (float) (-Math.PI/2);
+			}
+		}
+		
+		upCircleTheta -=  (double) (Math.asin((deltaY/viewCircleLength)));
+		
+		
+		double ZViewMove = (viewCircleLength-(viewCircleLength*Math.cos(viewCircleTheta)))+(playerCoords.getZ()-viewCircleLength);
+		double XViewMove = (viewCircleLength*Math.sin(viewCircleTheta))+playerCoords.getX();
+		double YViewMove = (viewCircleLength*Math.sin(upCircleTheta))+playerCoords.getY();
+		
+		playerLookAt.set(XViewMove,YViewMove, ZViewMove);
+		viewt3d.lookAt(playerCoords, playerLookAt, playerUp);
+		viewt3d.invert();
+		viewTrans.setTransform(viewt3d);
+	}
+	
+public Vector3d getCollPos(Vector3d vec1){
+		
+		int collX = (int) ((vec1.getX()+1)/2);
+		int collY = (int) ((vec1.getY()-2)/2);
+		int collZ = (int) ((vec1.getZ()+1)/2);
+		Vector3d vec2 = new Vector3d(new Point3d(collX,collY,collZ));
+		return vec2;
+	}
+	
+	
+	public void movePerson(int dim, double scale, int yDims){
+		boolean checkNext;
+		switch(dim){
+		case 1:
+	
+			Vector3d nextMovement = new Vector3d(playerCoords.getX()+Math.cos(viewCircleTheta)*scale*3,playerCoords.getY(),
+			playerCoords.getZ()+Math.sin(viewCircleTheta)*scale*3);
+			Vector3d collNext = getCollPos(nextMovement);
+			
+			
+		
+			try{
+				
+				 if(Arr.get((int) collNext.getZ()).get((int) ((yDims-1)-collNext.getY())).get((int) collNext.getX())!=0){
+			    	checkNext = false;
+			    }
+			    else{
+			    	checkNext = true;
+			    }	
+			}
+			catch(IndexOutOfBoundsException e){
+				checkNext = true;
+			}
+	
+			
+			if(checkNext){
+			
+				
+				playerCoords.add(new Point3d(Math.cos((viewCircleTheta))*scale,0,Math.sin((viewCircleTheta))*scale));
+				viewt3d.lookAt(playerCoords, playerLookAt, playerUp);
+				viewt3d.invert();
+				viewt3d.setTranslation(new Vector3d(playerCoords.getX(),playerCoords.getY(),playerCoords.getZ()));
+				viewTrans.setTransform(viewt3d);
+				
+			}
+			break;
+		
+		case 3:
+			
+			Vector3d nextMovement2 = new Vector3d(playerCoords.getX()-Math.sin(viewCircleTheta)*scale*3,playerCoords.getY(),
+					playerCoords.getZ()+Math.cos(viewCircleTheta)*scale*3);
+					Vector3d collNext2 = getCollPos(nextMovement2);
+					
+					
+				
+					try{
+						
+						 if(Arr.get((int) collNext2.getZ()).get((int) ((yDims-1)-collNext2.getY())).get((int) collNext2.getX())!=0){
+					    	checkNext = false;
+					    }
+					    else{
+					    	checkNext = true;
+					    }	
+					}
+					catch(IndexOutOfBoundsException e){
+						checkNext = true;
+					}
+			
+					
+					if(checkNext){
+					
+						
+						playerCoords.add(new Point3d(-Math.sin((viewCircleTheta))*scale,0,Math.cos((viewCircleTheta))*scale));
+						viewt3d.lookAt(playerCoords, playerLookAt, playerUp);
+						viewt3d.invert();
+						viewt3d.setTranslation(new Vector3d(playerCoords.getX(),playerCoords.getY(),playerCoords.getZ()));
+						viewTrans.setTransform(viewt3d);
+						
+					}
+			break;
+			}
+		
+	}
+	
+	
+	public void keyPressed(KeyEvent e) {
+		int k = e.getKeyCode();
+		if(!keyArr.contains(k)){
+			keyArr.add(k);
+		}	
+	}
+	
+	
+	public void keyReleased(KeyEvent e) {
+		int k = e.getKeyCode();
+		filter.var1 = k;
+		keyArr.removeIf(filter);
+	}
+	
+	public class keyRemove<T> implements Predicate<T>{
+
+		T var1;
+		
+		@Override
+		public boolean test(T var) {
+			if(var1.equals(var)){
+				return true;
+			}
+			else{
+				return false;
+			}	
+		}
+	}
+
+	public void KeyUpdate(){
+		
+		if(!(keyArr.size() ==0)){
+			for(int i = 0; i < keyArr.size();i++){
+				if(keyArr.get(i) == KeyEvent.VK_W){
+					if(keyArr.contains(KeyEvent.VK_SHIFT)){
+						movePerson(3,-1.4, yDim);
+					}
+					else{
+						movePerson(3,-0.4, yDim);
+					}
+				}
+				if(keyArr.get(i) == KeyEvent.VK_A){
+					
+					if(keyArr.contains(KeyEvent.VK_SHIFT)){
+						movePerson(1,-1.4, yDim);
+					}
+					else{
+						movePerson(1,-0.4, yDim);
+					}
+					
+				}
+				if(keyArr.get(i) == KeyEvent.VK_S){
+					if(keyArr.contains(KeyEvent.VK_SHIFT)){
+						movePerson(3,1.4, yDim);
+					}
+					else{
+						movePerson(3,0.4, yDim);
+					}
+					
+				}
+				if(keyArr.get(i) == KeyEvent.VK_D){
+					if(keyArr.contains(KeyEvent.VK_SHIFT)){
+						movePerson(1,1.4, yDim);
+					}
+					else{
+						movePerson(1,0.4, yDim);
+					}
+				}
+				if(keyArr.get(i) == KeyEvent.VK_SPACE){
+					
+					if(!alreadyJumped){
+						jumping = true;
+					}
+				}
+			}
+		}
+	}	
+}
+
